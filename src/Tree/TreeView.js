2b1c31527da840f1d7b8cee8fa83f8e2b90162e7
Mindaugas Bujanauskas <bujanauskas.m@gmail.com>
2018-07-24 13:35:57 +0300

    It's now possible to render the Tree recursively/iteratively
    Defaults to Recursive.

--- src/Tree/TreeView.js
+++ src/Tree/TreeView.js
@@ -1,9 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import './TreeView.css';
+
+export const MSG_NO_DATA = 'No tree data is loaded.';
 
 export default class TreeView extends Component {
-  render() {
+  static defaultProps = {
+    data: null
+  };
+
+  renderRecursively() {
+    if ( ! this.props.data.label) return null;
+    return this.renderBranchRecursively(this.props.data);
+  }
+
+  renderBranchRecursively(node) {
     return (
-      null
+      <div className="tree-branch" key={ node.label }>
+        { node.label }
+
+        {
+          !! node.children.length &&
+          node.children.map(node => this.renderBranchRecursively(node))
+        }
+      </div>
     );
+  }
+
+  renderIteratively() {
+    if ( ! this.props.data.label) return null;
+
+    const queue = [{ depth: 0, data: this.props.data }];
+    const nodes = [];
+
+    while (queue.length) {
+      const node = queue.pop();
+      nodes.push(node);
+
+      if (node.data.children.length) {
+        const children = node.data.children.map(child => ({ depth: node.depth + 1, data: child }));
+        queue.push(...children);
+      }
+    }
+
+    return nodes.map(node => {
+      let spacing = Array(node.depth).join('&nbsp;&nbsp;') + ' - ';
+
+      return (
+        <Fragment key={ node.data.label }>
+          <span dangerouslySetInnerHTML={ { __html: spacing } } />
+          { node.data.label }
+          <br/>
+        </Fragment>
+      );
+    });
+  }
+
+  render() {
+    if ( ! this.props.data) {
+      return MSG_NO_DATA;
+    }
+
+    return this.renderRecursively();
   }
 }
--- src/Tree/TreeView.test.js
+++ src/Tree/TreeView.test.js
@@ -1,7 +1,38 @@
 import React from 'react';
-import TreeView from './TreeView';
+import TreeView, { MSG_NO_DATA } from './TreeView';
+import treeJson from './tree';
 
 it('renders without crashing', () => {
   const wrapper = shallow(<TreeView />);
   expect(wrapper).toHaveLength(1);
+});
+
+it('has reasonable defaultProps', () => {
+  expect(TreeView.defaultProps.data).toBeNull();
+});
+
+it('renders a message when no tree data exists', () => {
+  const wrapper = shallow(<TreeView />);
+  expect(wrapper.text()).toBe(MSG_NO_DATA);
+});
+
+describe('with data', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<TreeView data={ treeJson } />);
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+    wrapper = null;
+  });
+
+  it('renders recursively', () => {
+    expect(wrapper.instance().renderRecursively()).toMatchSnapshot();
+  });
+
+  it('render iteratively', () => {
+    expect(wrapper.instance().renderIteratively()).toMatchSnapshot();
+  });
 });
