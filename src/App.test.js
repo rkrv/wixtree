7d2909228174448b9a6d5acbf28e1794e8c4d5e5
Mindaugas Bujanauskas <bujanauskas.m@gmail.com>
2018-07-24 12:22:21 +0300

    DRY App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,21 @@ import React from 'react';
 import App from './App';
 import TreeContainer from './Tree/TreeContainer';
 
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<App />);
+});
+
+afterEach(() => {
+  wrapper.unmount();
+  wrapper = null;
+});
+
 it('renders without crashing', () => {
-  const wrapper = shallow(<App />);
   expect(wrapper).toHaveLength(1);
 });
 
 it('renders <TreeContainer />', () => {
-  const wrapper = shallow(<App />);
   expect(wrapper.find(TreeContainer)).toHaveLength(1);
 });
