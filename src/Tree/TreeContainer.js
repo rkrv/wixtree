af1ac4d59b1121fe68011068699f1ab1e1d00377
Mindaugas Bujanauskas <bujanauskas.m@gmail.com>
2018-07-24 12:22:50 +0300

    TreeContainer loads the data

--- src/Tree/TreeContainer.js
+++ src/Tree/TreeContainer.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import treeJson from './tree';
 
 export default class TreeContainer extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    this.setState({ data: treeJson });
+  }
+
   render() {
     return (
       null
--- src/Tree/TreeContainer.test.js
+++ src/Tree/TreeContainer.test.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import TreeContainer from './TreeContainer';
+import treeJson from './tree';
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<TreeContainer />);
+});
+
+afterEach(() => {
+  wrapper.unmount();
+  wrapper = null;
+});
 
 it('renders without crashing', () => {
-  const wrapper = shallow(<TreeContainer />);
   expect(wrapper).toHaveLength(1);
+});
+
+it('starts with reasonable defaults', () => {
+  expect(wrapper.state('data')).toEqual(treeJson);
 });
